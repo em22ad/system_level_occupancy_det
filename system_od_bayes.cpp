8b9cd1ad7ee0ab6517001507d6f5b20fbad98608
Your Name <you@example.com>
2022-02-03 23:10:14 -0500

    some updates with macro and usage

--- system_od_bayes.cpp
+++ system_od_bayes.cpp
@@ -50,6 +50,23 @@ static double argInit_real_T()
 // Arguments    : void
 // Return Type  : void
 //
+/*
+ * Pi4 board, 4GHz
+ * timestamp is the period for each node's detection
+ * e.g.{
+ *  timestamp:0,    [0,0,0,0]
+ *  timestamp:40,   [0,1,1,0]
+ * }
+ *
+ * delay { Pi delay test
+ *  3: bayes_filter(timestamp, dv, *(cell_wrap_0(*)[1]) & e_label); < 10ms
+ *  4: good
+ *  5: good
+ *  6: good
+ * }
+ *
+ *
+ **/
 static void main_bayes_filter()
 {
     cell_wrap_0 e_label;
@@ -62,6 +79,10 @@ static void main_bayes_filter()
     dv[0] = 0; //sensor X1 output = 0
     dv[1] = 0; //sensor X2 output = 0
     dv[2] = 0; //sensor X3 output = 0
+    //bool dv_out[3];
+    //[UNOCC, X1, X2, X3, {X1,X2}, {X1,X3}, {X2,X3}, {X1,X2,X3}]
+    // 0,     1,  1,  1,   1,      1,        1,       1
+    //bayes_filter(timestamp, 3, dv, dv_out);
     timestamp = 0;
     bayes_filter(timestamp, dv, *(cell_wrap_0(*)[1]) & e_label);
     cout << e_label.f1.data <<"\n";
@@ -117,6 +138,7 @@ static void main_bayes_filter()
     //bayes_filter(argInit_real_T(), dv, *(cell_wrap_0(*)[1]) & e_label);
 }
 
+#ifndef SYSTEM_OD_BAYES_INTEGRATION
 int main(int, char **)
 {
     main_bayes_filter();
@@ -125,6 +147,7 @@ int main(int, char **)
     bayes_filter_terminate();
     return 0;
 }
+#endif // SYSTEM_OD_BAYES_INTEGRATION
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
